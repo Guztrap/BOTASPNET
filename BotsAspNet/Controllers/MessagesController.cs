5f75280e5da6c450b0d5b9f3b704824d3b5c4841
Gustavo David Ramirez Ledesma <gramirez@netintellego.com>
2019-01-07 09:46:48 -0600

    First commit

---
+++ BotsAspNet/Controllers/MessagesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+
+namespace BotsAspNet.Controllers
+{ 
+    [BotAuthentication]
+    public class MessagesController : ApiController
+    {
+        public async Task<HttpResponseMessage> Post([FromBody] Activity activity)
+        {
+            if (activity.Type == ActivityTypes.Message)
+            {
+                await Conversation.SendAsync(activity, () => new Dialogs.MyDialog());
+            }
+            else
+            {
+                HandleSystemMessage(activity);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK);            
+        }
+
+        private void HandleSystemMessage(Activity message)
+        {
+            switch (message.Type)
+            {
+                case ActivityTypes.ConversationUpdate: //Indica que el bot fue agregado a la conversacion u otros miembros se agregaron o quitaron
+                    break;
+                case ActivityTypes.ContactRelationUpdate: //Bot fue agregado o eliminado de la lista de contactos
+                    break;
+                case ActivityTypes.Typing: //indica que el opuesto esta escribiendo
+                    break;
+                case ActivityTypes.Ping: // Checa si el endpoint esta accesible
+                    break;
+                case ActivityTypes.DeleteUserData: //el usuario pidio que se eliminara toda la informacion almacenada.
+                    break;
+            }
+        }
+    }
+}
---
+++ BotsAspNet/Dialogs/MyDialog.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.Bot.Builder.Dialogs;
+using Microsoft.Bot.Connector;
+using System.Threading.Tasks;
+
+namespace BotsAspNet.Dialogs
+{
+    [Serializable]
+    public class MyDialog : IDialog<string>
+    {
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task MessageReceivedAsync(IDialogContext context, IAwaitable<object> result)
+        {
+            var activityMessage = await result as Activity;
+            var message = activityMessage.Text;
+
+            if (message.Contains("Assortment"))
+            {
+                await context.PostAsync("It´s an application designed for Ann Inc in order to create the season plan");
+            }
+            else
+            {
+                await context.PostAsync($"Recibiste: {message}");
+            }
+            
+            context.Wait(MessageReceivedAsync);
+        }
+    }
+}
